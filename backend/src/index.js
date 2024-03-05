4a8468198dda24fe8f53b4f16c60e26b712dc917
Michal B <m.borowa.2003@gmail.com>
2024-03-05 18:47:47 +0100

    init commit

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+const appPort = process.env.APP_PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${appPort}`);
+})
