411b48a96ea3c098a9d997f62bf9a657bbbfe39e
Michal B <m.borowa.2003@gmail.com>
2024-03-07 00:09:32 +0100

    tailwindCSS init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/Main.jsx'
+import Test from "./pages/Test.jsx";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<Main />} path="/" />
+				<Route element={<Test />} path="/test" />
 			</Routes>
 		</BrowserRouter>
 	)
