dc281edd35e7dc29333f6cabddd798baae1caf54
Michal B <m.borowa.2003@gmail.com>
2024-03-26 01:02:13 +0100

    Validation of user edition

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -15,7 +15,7 @@ export const getUsers = async (req, res) => {
 export const getUserData = async (req, res) => {
     try {
         const { login } = req.params
-        const user = await User.findOne({ login })
+        const user = await User.findOne({ login }).select("-password")
 
         if (!user) {
             return res.status(404).json({ msg: `User with ${login} not found.` })
@@ -61,55 +61,28 @@ export const searchUsers = async (req, res) => {
     }
 }
 
-const updateUser = async (filter, update) => {
-    try {
-        // Construct the update object with non-empty fields
-        const nonEmptyUpdate = {};
-
-        if (update.password) {
-            const salt = await bcrypt.genSalt(10)
-            const hashedPassword = await bcrypt.hash(update.password, salt)
-            update.password = hashedPassword
-        }
-
-        const arrayOfLocations = ['city', 'postalCode', 'street', 'propertyNumber', 'apartmentNumber'];
-
-        for (const key in update) {
-            console.log(`Key ${key}`)
-            if (key == 'location') {
-                const dbLocation = await User.findOne(filter, 'location');
-
-                const locationUpdate = { ...dbLocation.location }
-
-                // Iterate over the keys of the location object in the update
-                for (const field in update.location) {
-                    if (arrayOfLocations.includes(field) && update.location[field] !== '') {
-                        locationUpdate[field] = update.location[field];
-                    }
-                }
-
-                nonEmptyUpdate.location = locationUpdate
-            } else if (update[key] !== '') {
-                nonEmptyUpdate[key] = update[key];
-            }
-        }
-        console.log(nonEmptyUpdate)
-        const updatedUser = await User.findOneAndUpdate(filter, nonEmptyUpdate, { new: true })
-
-        return updatedUser;
-    } catch (error) {
-        console.error('Error updating user:', error);
-        throw error;
-    }
-}
-
 export const editUser = async (req, res) => {
     try {
         const { userData, previousLogin } = req.body
-        console.log(userData, previousLogin)
+        const { login, email, pesel } = userData
 
-        const query = { login: previousLogin }
-        const updatedUser = await updateUser(query, userData)
+        const emailUser = await User.findOne({ email: email, login: { $ne: previousLogin } })
+        if (emailUser) {
+            return res.status(403).json({ msg: `Email: ${email} already exists for another user.` });
+        }
+        const peselUser = await User.findOne({ pesel: pesel, login: { $ne: previousLogin } })
+        if (peselUser) {
+            return res.status(403).json({ msg: `PESEL: ${pesel} already exists for another user.` });
+        }
+
+        if (login !== previousLogin) {
+            const loginUser = await User.findOne({ login: login })
+            if (loginUser) {
+                return res.status(403).json({ msg: `Login: ${login} already exists for another user.` });
+            }
+        }
+
+        const updatedUser = await User.findOneAndUpdate({ login: previousLogin }, userData, { new: true })
 
         return res.status(201).json(updatedUser)
     } catch (error) {
--- frontend/src/components/EditTable.jsx
+++ frontend/src/components/EditTable.jsx
@@ -20,28 +20,87 @@ const EditProfilePage = () => {
         birthDate: "",
         gender: "",
         email: "",
-        phoneNumber: "",
-        password: ""
+        phoneNumber: ""
     })
 
-    const [previousUserData, setPreviousUserData] = useState({
-        login: "",
-        name: "",
-        lastName: "",
-        location: {
-            city: "",
-            postalCode: "",
-            street: "",
-            propertyNumber: "",
-            apartmentNumber: "",
-        },
-        pesel: "",
-        birthDate: "",
-        gender: "",
-        email: "",
-        phoneNumber: "",
-        password: ""
-    })
+    const calculatePESELControlDigit = (pesel) => {
+        const weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+        let sum = 0;
+
+        for (let i = 0; i < 10; i++) {
+            sum += (parseInt(pesel.charAt(i), 10) * weights[i]) % 10;
+        }
+
+        sum %= 10;
+        if (sum !== 0) {
+            sum = 10 - sum;
+        }
+
+        return sum;
+    }
+
+    const validatePESEL = (pesel) => {
+        if (pesel.length !== 11) {
+            return false;
+        }
+
+        const controlDigit = parseInt(pesel.charAt(10), 10);
+        const calculatedControlDigit = calculatePESELControlDigit(pesel);
+
+        return controlDigit === calculatedControlDigit;
+    }
+    function validateBirthDateWithPESEL(birthDate, pesel) {
+        // Extracting birth date from PESEL
+        const year = parseInt(pesel.substring(0, 2), 10) + (parseInt(pesel.substring(2, 3), 10) >= 2 ? 2000 : 1900);
+        const month = parseInt(pesel.substring(2, 4), 10) % 20; // Handle months for different centuries
+        const day = parseInt(pesel.substring(4, 6), 10);
+
+        // Formatting birthDate to match the format extracted from PESEL
+        const formattedBirthDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+
+        return birthDate === formattedBirthDate;
+    }
+
+    function validateGenderWithPESEL(gender, pesel) {
+        const genderDigit = parseInt(pesel[9], 10);
+        const isMale = genderDigit % 2 === 1;
+        return (gender === 'M' && isMale) || (gender === 'W' && !isMale);
+    }
+
+    const validateFormData = () => {
+
+        // Postal code validation (xx-xxx format)
+        if (userData.postalCode && !/^\d{2}-\d{3}$/.test(userData.postalCode)) {
+            alert('Invalid postal code format. Expected format: XX-XXX.')
+            return false
+        }
+
+        // Phone number length check
+        if (userData.phoneNumber && userData.phoneNumber.length !== 9) {
+            alert('Phone number must be 9 digits long.')
+            return false
+        }
+
+        // PESEL validation
+        if (userData.pesel && !validatePESEL(userData.pesel)) {
+            alert('Invalid PESEL.')
+            return false
+        }
+
+        // Birth date validation with PESEL
+        if (userData.pesel && userData.birthDate && !validateBirthDateWithPESEL(userData.birthDate, userData.pesel)) {
+            alert('Birth date does not match the date in PESEL.')
+            return false
+        }
+
+        // Gender validation with PESEL
+        if (userData.pesel && userData.gender && !validateGenderWithPESEL(userData.gender, userData.pesel)) {
+            alert('Gender does not match the gender in PESEL.');
+            return false
+        }
+
+        return true
+    }
 
     const location = useLocation();
     const query = new URLSearchParams(location.search).get('login')
@@ -49,21 +108,31 @@ const EditProfilePage = () => {
     useEffect(() => {
         const getUser = async () => {
             const response = await axios.get(`http://localhost:3000/backend/admin/user-data/${query}`)
-            setPreviousUserData(response.data)
+            setUserData(response.data)
         }
 
         getUser()
-        console.log(previousUserData)
+        console.log(userData)
     }, []);
 
-
+    useEffect(() => {
+        // Convert MongoDB date to YYYY-MM-DD format for the input field when component mounts or the date changes
+        if (userData.birthDate) {
+            const formattedDate = new Date(userData.birthDate).toISOString().split('T')[0];
+            setUserData((prevData) => ({
+                ...prevData,
+                birthDate: formattedDate,
+            }));
+        }
+    }, [userData.birthDate]);
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        const checkData = validateFormData()
+        if (checkData == false) return
         try {
-            const { login: previousLogin } = previousUserData
+            const previousLogin = query
 
             const response = await axios.post(`http://localhost:3000/backend/admin/edit-user`, { userData, previousLogin }, { withCredentials: true });
             if (response.status == 201) {
@@ -71,7 +140,11 @@ const EditProfilePage = () => {
                 window.location.reload()
             }
         } catch (error) {
-            console.error("Error:", error);
+            console.error("Error:", error)
+            if (error.response.data.msg) {
+                alert(error.response.data.msg)
+                window.location.reload()
+            }
         }
     };
 
@@ -102,69 +175,65 @@ const EditProfilePage = () => {
             <form className="EditProfile-form" onSubmit={handleSubmit}>
                 <div className="form-column">
                     <div className="form-group">
-                        <label>{`Current username: ${previousUserData.login}` || ""}</label>
-                        <label htmlFor="login" className="font-bold">Username</label>
+                        <label htmlFor="login" className="font-bold">Username*</label>
                         <input
                             type="text"
                             id="login"
                             name="login"
                             value={userData.login}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current name: ${previousUserData.name}` || ""}</label>
-                        <label htmlFor="name" className="font-bold">First Name</label>
+                        <label htmlFor="name" className="font-bold">First Name*</label>
                         <input
                             type="text"
                             id="name"
                             name="name"
                             value={userData.name}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current lastName: ${previousUserData.lastName}` || ""}</label>
-                        <label htmlFor="lastName" className="font-bold">Last Name</label>
+                        <label htmlFor="lastName" className="font-bold">Last Name*</label>
                         <input
                             type="text"
                             id="lastName"
                             name="lastName"
                             value={userData.lastName}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current city: ${previousUserData.location.city}` || ""}</label>
-                        <label htmlFor="city" className="font-bold">City</label>
+                        <label htmlFor="city" className="font-bold">City*</label>
                         <input
                             type="text"
                             id="city"
                             name="city"
                             value={userData.location.city}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current city: ${previousUserData.location.postalCode}` || ""}</label>
-                        <label htmlFor="postalCode" className="font-bold">Postal Code</label>
+                        <label htmlFor="postalCode" className="font-bold">Postal Code*</label>
                         <input
                             type="text"
                             id="postalCode"
                             name="postalCode"
                             value={userData.location.postalCode}
                             onChange={handleChange}
-
+                            required
+                            pattern="[0-9]{2}-[0-9]{3}"
+                            title="Postal code must be in the format XX-XXX"
                         />
                     </div>
                 </div>
                 <div className="form-column">
                     <div className="form-group">
-                        <label>{`Current street: ${previousUserData.location.street}` || ""}</label>
                         <label htmlFor="street" className="font-bold">Street</label>
                         <input
                             type="text"
@@ -175,19 +244,17 @@ const EditProfilePage = () => {
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current property number: ${previousUserData.location.propertyNumber}` || ""}</label>
-                        <label htmlFor="propertyNumber" className="font-bold">Property Number</label>
+                        <label htmlFor="propertyNumber" className="font-bold">Property Number*</label>
                         <input
                             type="text"
                             id="propertyNumber"
                             name="propertyNumber"
                             value={userData.location.propertyNumber}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current apartment number: ${previousUserData.location.apartmentNumber}` || ""}</label>
                         <label htmlFor="apartmentNumber" className="font-bold">Apartment Number</label>
                         <input
                             type="text"
@@ -198,87 +265,71 @@ const EditProfilePage = () => {
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current pesel: ${previousUserData.pesel}` || ""}</label>
-                        <label htmlFor="pesel" className="font-bold">PESEL Number</label>
+                        <label htmlFor="pesel" className="font-bold">PESEL Number*</label>
                         <input
                             type="text"
                             id="pesel"
                             name="pesel"
                             value={userData.pesel}
                             onChange={handleChange}
-
+                            required
+                            pattern="[0-9]{11}"
+                            title="PESEL must contain exactly 11 digits"
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current birth date: ${previousUserData.birthDate}` || ""}</label>
-                        <label htmlFor="birthDate" className="font-bold">Birth Date</label>
+                        <label htmlFor="birthDate" className="font-bold">Birth Date*</label>
                         <input
                             type="date"
                             id="birthDate"
                             name="birthDate"
                             value={userData.birthDate}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                 </div>
                 <div className="form-column">
                     <div className="form-group">
-                        <label>{`Current email: ${previousUserData.email}` || ""}</label>
-                        <label htmlFor="email" className="font-bold">Email Address</label>
+                        <label htmlFor="email" className="font-bold">Email Address*</label>
                         <input
                             type="email"
                             id="email"
                             name="email"
                             value={userData.email}
                             onChange={handleChange}
-
+                            required
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current phone number: ${previousUserData.phoneNumber}` || ""}</label>
-                        <label htmlFor="phoneNumber" className="font-bold">Phone Number</label>
+                        <label htmlFor="phoneNumber" className="font-bold">Phone Number*</label>
                         <input
                             type="tel"
                             id="phoneNumber"
                             name="phoneNumber"
                             value={userData.phoneNumber}
                             onChange={handleChange}
-
+                            required
+                            pattern="[0-9]{9}"
+                            title="Phone number must contain exactly 9 digits"
                         />
                     </div>
                     <div className="form-group">
-                        <label>{`Current gender: ${previousUserData.gender}` || ""}</label>
-                        <label htmlFor="gender" className="font-bold">Gender</label>
+                        <label htmlFor="gender" className="font-bold">Gender*</label>
                         <select
                             id="gender"
                             name="gender"
                             value={userData.gender}
                             onChange={handleChange}
-
+                            required
                         >
                             <option value="W">Woman</option>
                             <option value="M">Male</option>
                         </select>
                     </div>
-                    <div className="form-group">
-                        <label>Only change if needed</label>
-                        <label htmlFor="password" className="font-bold">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={userData.password}
-                            onChange={handleChange}
-
-                        />
-                    </div>
-                    <br />
-
-
                 </div>
+                <div className="edit-button"> <button type="submit">Submit Changes</button></div>
             </form>
-            <div className="edit-button"> <button type="submit">Submit Changes</button></div>
 
         </div>
 
