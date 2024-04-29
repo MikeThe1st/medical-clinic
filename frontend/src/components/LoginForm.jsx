d37bc1c12c346369816aa2b98fb942bee32f948a
Michal B <m.borowa.2003@gmail.com>
2024-04-29 18:56:16 +0200

    Adding blocking system after 3 or more failed login attempts

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'; // Załóżmy, że używamy React Router
 
@@ -6,8 +6,24 @@ import "../css/Login.css";
 
 const LoginForm = () => {
 
+    const [failedLogins, setFailedLogins] = useState(0)
+    const [ableToLogin, setAbleToLogin] = useState(true)
+
+    useEffect(() => {
+        if (failedLogins >= 3) {
+            setAbleToLogin(false)
+            const timeoutId = setTimeout(() => {
+                setAbleToLogin(true)
+            }, 10000)
+
+            return () => clearTimeout(timeoutId)
+        }
+    }, [failedLogins])
+
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        if (!ableToLogin) return alert('You still cannot login! Wait few seconds before trying again.')
 
         const formData = new FormData(e.target)
 
@@ -29,15 +45,19 @@ const LoginForm = () => {
             else alert(response.data)
         } catch (error) {
             console.error('Error:', error)
+            setFailedLogins(failedLogins + 1)
+            alert(`Failed logins ${failedLogins}`)
             const message = error.response.data.msg
             if (message) {
                 alert(message)
             }
+            if (failedLogins >= 3) return alert(`You cannot login for the next 10 seconds, because of ${failedLogins} failed attempts.`)
         }
     }
 
     const loginAsUser = async (e) => {
         e.preventDefault()
+        if (!ableToLogin) return alert('You still cannot login! Wait few seconds before trying again.')
         try {
             const response = await axios.post('http://localhost:3000/backend/user/login', { email: 'user@gmail.com', password: 'user123' }, { withCredentials: true })
             if (response.status === 200) {
@@ -58,6 +78,7 @@ const LoginForm = () => {
 
     const loginAsAdmin = async (e) => {
         e.preventDefault()
+        if (!ableToLogin) return alert('You still cannot login! Wait few seconds before trying again.')
         try {
             const response = await axios.post('http://localhost:3000/backend/user/login', { email: 'admin@gmail.com', password: 'admin123' }, { withCredentials: true })
             if (response.status === 200) {
